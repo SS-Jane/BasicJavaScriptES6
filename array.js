ede29cf9a45acab880b872a26d3a11e3ee6e9ab9
SS-Jane <superjane.prodev@gmail.com>
2024-07-25 10:23:48 +0700

    add array

---
+++ array.js
@@ -0,0 +1,13 @@
+// join() array --> convert array to string
+
+const data1 = [100, 200, 300]
+const data2 = [400, 500]
+
+console.log(data1)
+
+const result = data1.join()
+console.log(result)
+
+// concat() --> join array
+const alldata = data1.concat(data2)
+console.log(alldata);
