f10d3f419dbe6dbf0a6eee7ed7cb4a003fa5122f
SS-Jane <superjane.prodev@gmail.com>
2024-08-26 16:46:59 +0700

    add finished dark&light mode

--- ex/DarkandLightmode/script.js
+++ ex/DarkandLightmode/script.js
@@ -1,15 +1,19 @@
 const switchToggle = document.querySelector('input[type="checkbox"]')
 const toggleIcon = document.getElementById('toggle-icon');
 const nav = document.getElementById('nav');
-
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
 
 function switchMode(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme','dark');
         darkMode();
+        switchImgMode('dark');
     } else {
         document.documentElement.setAttribute('data-theme','light');
         lightMode();
+        switchImgMode('light');
     }
     
 };
@@ -26,6 +30,13 @@ function lightMode() {
     toggleIcon.children[0].textContent='Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    
+}
+
+function switchImgMode(mode) {
+    img1.src=`/DarkandLightmode/img/undraw_website_${mode}.svg`
+    img2.src=`/DarkandLightmode/img/undraw_freelancer_${mode}.svg`
+    img3.src=`/DarkandLightmode/img/undraw_projections_${mode}.svg`
     
 }
 switchToggle.addEventListener('change',switchMode)
